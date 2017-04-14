3d824e9a8856223b2fe7ada10d7d4fa310db3a21
OptimisticShibe <britishdragons123@gmail.com>
2017-04-13 20:45:41 -0700

    Update in-class

--- HelloWorld/Controllers/HomeController.cs
+++ HelloWorld/Controllers/HomeController.cs
@@ -9,31 +9,49 @@ namespace HelloWorld.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Products()
-        {
-            var products = new Product[]
-            {
-        new Product{ ProductId = 1, Name = "First One", Price = 1.11m, Count = 2},
-        new Product{ ProductId = 2, Name="Second One", Price = 2.22m, Count = 1},
-        new Product{ ProductId = 3, Name="Third One", Price = 3.33m, Count = 0},
-        new Product{ ProductId = 4, Name="Fourth One", Price = 4.44m, Count = 5},
-            };
+        //public ActionResult Products()
+        //{
+        //    var products = new Product[]
+        //    {
+        //new Product{ ProductId = 1, Name = "First One", Price = 1.11m, Count = 2},
+        //new Product{ ProductId = 2, Name="Second One", Price = 2.22m, Count = 1},
+        //new Product{ ProductId = 3, Name="Third One", Price = 3.33m, Count = 0},
+        //new Product{ ProductId = 4, Name="Fourth One", Price = 4.44m, Count = 5},
+        //    };
 
-            return View(products);
+        //    return View(products);
+        //}
+
+        //public ActionResult Product()
+        //{
+        //    var myProduct = new Product
+        //    {
+        //        ProductId = 1,
+        //        Name = "Kayak",
+        //        Description = "A boat for one person",
+        //        Category = "water-sports",
+        //        Price = 200m,
+        //    };
+
+        //    return View(myProduct);
+        //}
+
+
+        private IProductRepository productRepository;
+
+        public HomeController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
         }
 
         public ActionResult Product()
         {
-            var myProduct = new Product
-            {
-                ProductId = 1,
-                Name = "Kayak",
-                Description = "A boat for one person",
-                Category = "water-sports",
-                Price = 200m,
-            };
+            return View(productRepository.Products.First());
+        }
 
-            return View(myProduct);
+        public ActionResult Products()
+        {
+            return View(productRepository.Products);
         }
 
         // GET: Home
