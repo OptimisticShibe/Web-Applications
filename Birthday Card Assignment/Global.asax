d9f9544e741f9a698565c257efea2ff74f7f3f53
OptimisticShibe <Rafiq@Rafiqs-MacBook-Pro.local>
2017-04-13 20:23:17 -0700

    First commit

---
+++ Birthday Card Assignment/Global.asax
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace HelloWorld
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        // To handle exception for the entire application
+        protected void Application_Error()
+        {
+            var exception = Server.GetLastError();
+
+            Server.ClearError();
+
+            var routeData = new RouteData();
+            routeData.Values.Add("controller", "Error");
+            routeData.Values.Add("action", "Error");
+
+            IController errorController = new Controllers.ErrorController();
+            errorController.Execute(new RequestContext(new HttpContextWrapper(Context), routeData));
+        }
+
+    }
+}
---
+++ HelloWorld/Global.asax.cs
@@ -0,0 +1,49 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Reflection; // Advanced C# topic (backend). Add this for assembly
+using Autofac;
+using Autofac.Integration.Mvc;
+
+namespace HelloWorld
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            RegisterAutofac(); // as application starts, Autofac gets called
+        }
+
+        // Autofac registration
+        private void RegisterAutofac()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly()).AsSelf().AsImplementedInterfaces();
+
+            //builder.RegisterType<ContactRepository>().As<IContactRepository>();
+
+            var container = builder.Build();
+
+            // Configure dependency resolver.
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+
+        protected void Application_Error()
+        {
+            var exception = Server.GetLastError();
+            Server.ClearError();
+
+            var routeData = new RouteData();
+            routeData.Values.Add("controller", "Error");
+            routeData.Values.Add("action", "Error");
+
+            IController errorController = new Controllers.ErrorController();
+            errorController.Execute(new RequestContext(new HttpContextWrapper(Context), routeData));
+        }
+    }
+}
