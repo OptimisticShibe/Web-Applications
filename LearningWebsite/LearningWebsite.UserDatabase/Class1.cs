898df2b0883c6707b8459a73776d78bc8f61a75f
OptimisticShibe <britishdragons123@gmail.com>
2017-05-01 19:27:49 -0700

    Uploading progress made while in class.

---
+++ LearningWebsite/LearningWebsite.UserDatabase/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningWebsite.UserDatabase
+{
+    public class Class1
+    {
+    }
+}
--- LearningWebsite/LearningWebsite/Global.asax.cs
+++ LearningWebsite/LearningWebsite/Global.asax.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
+using System.Reflection;
 
 namespace LearningWebsite
 {
@@ -16,6 +19,37 @@ namespace LearningWebsite
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            RegisterAutofac();
+        }
+
+        private void RegisterAutofac()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+            RegisterAssemblyTypes(builder, Assembly.GetExecutingAssembly());
+
+            var container = builder.Build();
+
+            // Configure dependency resolver.
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+
+        private void RegisterAssemblyTypes(ContainerBuilder builder, Assembly assembly)
+        {
+            builder.RegisterAssemblyTypes(assembly).AsSelf().AsImplementedInterfaces();
+
+            var assemblyNames = assembly.GetReferencedAssemblies();
+
+            foreach (var assemblyName in assemblyNames)
+            {
+                if (assemblyName.FullName.ToLower().Contains("LearningWebsite"))
+                {
+                    assembly = Assembly.Load(assemblyName);
+                    RegisterAssemblyTypes(builder, assembly);
+                }
+            }
         }
     }
 }
